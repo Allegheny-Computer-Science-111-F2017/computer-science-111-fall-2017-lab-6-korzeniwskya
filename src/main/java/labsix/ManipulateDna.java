ac08487c5d7a434ee713719bc3417e165ec359be
Alex Korzeniwsky <AlexKorzeniwsky@Alexs-MacBook-Pro.local>
2017-10-12 18:41:17 -0400

    Program done, just needs reflection and gatorgrader.

--- src/main/java/labsix/ManipulateDna.java
+++ src/main/java/labsix/ManipulateDna.java
@@ -28,20 +28,22 @@ public class ManipulateDna {
     }
     // Step One: Read in and display the chosen DNA string
     String dna = scanner.nextLine();
+    dna = dna.toUpperCase();
     System.out.println("We will be working with a string of DNA: \"" + dna + "\"");
     // Step Two: Compute the complement of the DNA String
+    dna = dna.toLowerCase();
     String dnaMod = dna;
     dnaMod = dnaMod.replace('a', 'T');
     dnaMod = dnaMod.replace('c', 'G');
     dnaMod = dnaMod.replace('t', 'A');
     dnaMod = dnaMod.replace('g', 'C');
-    dnaMod = dnaMod.toLowerCase();
+    dna = dna.toUpperCase();
     System.out.println("The complement to \"" + dna + "\" is \"" + dnaMod + "\"");
     // Step Three: Insert a randomly chosen DNA letter into the DNA String
     Random generator = new Random();
     int randNum1 = generator.nextInt(4);
 
-    String dnaInsert = dna.substring(0,randNum1) + "c" + dna.substring(randNum1);
+    String dnaInsert = dna.substring(0,randNum1) + "C" + dna.substring(randNum1);
     System.out.println("DNA with a random insertion may look like this: \"" + dnaInsert + "\"");
     // Step Four: Delete a DNA letter from a randomly chosen position in the DNA string
     StringBuilder dnaModOut = new StringBuilder(dna);
@@ -53,7 +55,9 @@ public class ManipulateDna {
     int randNum3 = generator.nextInt(4);
     char replaceChar = dna.charAt(randNum2);
     dnaRand.setCharAt(randNum3, replaceChar);
-    System.out.println(dnaRand);
+    System.out.println("By choosing a random char, \"" + replaceChar + "\"");
+    System.out.println("And placing \"" + replaceChar + "\" in a random location in the sequence,");
+    System.out.println("We get, \"" + dnaRand + "\"");
     // Step Six: Display a final thankyou message
     System.out.println("Thank you for experimenting with DNA!");
 
